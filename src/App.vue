bb0ea7217a5f85a811724d462736f69ca6905ca2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 01:20:14 -0600

    diseño de la pagina

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <nav>
+
+  <div id="nav">
+    <Navbar/>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </div>
+  
   <router-view/>
+
 </template>
+
+
+<script>
+// 1er Paso: Importar el componente
+import Navbar from './components/Navbar.vue'
+
+// 2do Paso: Exportar el componente
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <img src="https://mms.businesswire.com/media/20210615005442/en/885230/23/Credijusto_NW_RGB-01-02_%282%29.jpg" alt="" width="100" height="60" class="d-inline-block align-text-top">
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
