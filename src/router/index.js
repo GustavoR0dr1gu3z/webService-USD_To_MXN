7cd8d6ac20d2cc90852700e2dfcc6555f4d34a1a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 01:01:37 -0600

    creando app y configurando rutas

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,43 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  
   {
-    path: '/about',
-    name: 'about',
+    path: '/api/',
+    name: 'api',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (api.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "api" */ '../views/Api.vue')
+  },
+
+  {
+    path: '/api/diarioOficialFederacion',
+    name: 'diarioOficialFederacion',
+    // route level code-splitting
+    // this generates a separate chunk (DiarioOficialFederacion.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "DiarioOficialFederacion" */ '../views/DiarioOficialFederacion.vue')
+  },
+
+  {
+    path: '/api/fixer',
+    name: 'fixer',
+    // route level code-splitting
+    // this generates a separate chunk (fixer.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "fixer" */ '../views/Fixer.vue')
+  },
+  
+  {
+    path: '/api/banxico',
+    name: 'banxico',
+    // route level code-splitting
+    // this generates a separate chunk (banxico.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "banxico" */ '../views/Banxico.vue')
+  }, 
+
 ]
 
 const router = createRouter({
---
+++ src/views/Banxico.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="banxico">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>Banxico</h1>
+    </div>
+</template>
