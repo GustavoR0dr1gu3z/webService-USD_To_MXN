eb82a06a98527ea4f98399cf96d22cb93b4a259f
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 12:04:43 -0600

    detalles arreglados

--- src/views/DiarioOficialFederacion.vue
+++ src/views/DiarioOficialFederacion.vue
@@ -8,7 +8,7 @@
             <div class="card-body">
                 <h5 class="card-title">Fecha de Última Modificación:</h5>
                 <p class="card-text">{{date}}</p>
-                <button disabled="disabled">{{rates}}</button>
+                <button disabled="disabled">${{rates}}</button>
             </div>
         </div>
     </div>
