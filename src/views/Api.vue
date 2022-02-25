67d49d0176a26a0715e6e975ffe7a93b2f29f13e
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 11:56:32 -0600

    editando view de api

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="api">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <h1>API</h1>
+
+        <Fixer/>
+
     </div>
 </template>
+
+<script>
+// 1er Paso: Importar el componente
+import Fixer from './Fixer.vue'
+
+// 2do Paso: Exportar el componente
+export default {
+  components: {
+    Fixer,
+  },
+}
+</script>
+
+<style scoped>
+
+    .api{
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+</style>
