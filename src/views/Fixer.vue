2e4eab12ec7670a1ced8d1b02a71c4b097afff22
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 11:06:26 -0600

    instalando axios para API

--- src/views/Fixer.vue
+++ src/views/Fixer.vue
@@ -2,5 +2,31 @@
     <div class="fixer">
         <img alt="Vue logo" src="../assets/logo.png">
         <h1>Fixer</h1>
+        <h3>{{message}}</h3>
     </div>
 </template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                message: 'Hello Vue!'
+            }
+        },
+        methods: {
+
+            async apiFixer(){
+                const response = await axios.get('http://data.fixer.io/api/latest?access_key=a047dec8343d84e4f5590a1db236e2ad');
+                console.log(response.data);
+            }
+
+        },
+
+        created(){
+            /* Created es un metodo para ejecutar simpre*/
+            this.apiFixer();
+        }
+
+    }
+</script>
