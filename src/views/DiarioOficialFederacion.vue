270ffc158e98e98f755cb2e88a628971bba60ee1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-02-25 01:06:39 -0600

    configurando rutas

---
+++ src/views/DiarioOficialFederacion.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="diarioOficialFederacion">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>diarioOficialFederacion</h1>
+    </div>
+</template>
---
+++ src/views/Fixer.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="fixer">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>Fixer</h1>
+    </div>
+</template>
